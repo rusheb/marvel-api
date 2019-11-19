4efa7611a3119b3a6a585fbc926ac23abc2fd449
rusheb <rusheb.shah@gmail.com>
2019-11-19 07:26:01 +0000

    Use a generator

--- main.py
+++ main.py
@@ -2,49 +2,52 @@ import json
 from datetime import datetime
 from hashlib import md5
 from os import environ
+from typing import Dict, Generator
 
 import requests
 import requests_cache
 
-URL = "http://gateway.marvel.com/v1/public/characters"
+REQUEST_URL = "http://gateway.marvel.com/v1/public/characters"
 PUBLIC_KEY = environ["MARVEL_PUBLIC_KEY"]
 PRIVATE_KEY = environ["MARVEL_PRIVATE_KEY"]
 
 
-def get_characters(offset: int):
+def get_all_characters(offset: int = 0) -> Generator[Dict, None, None]:
+    characters = None
+    while characters != []:
+        try:
+            characters = get_characters_chunk(offset)
+            for character in characters:
+                yield character
+
+            offset += 100
+        except Exception:
+            raise
+
+
+def get_characters_chunk(offset: int) -> Dict:
     print(f"Getting characters {offset} to {offset + 100}")
 
     now = datetime.now().strftime("%b %d %Y %H:%M:%S")
-    string_to_hash = now + PRIVATE_KEY + PUBLIC_KEY
-    request_hash = md5(string_to_hash.encode("utf-8")).hexdigest()
+    hash_param = md5(f"{now}{PRIVATE_KEY}{PUBLIC_KEY}".encode("utf-8")).hexdigest()
 
     params = {
         "apikey": PUBLIC_KEY,
         "ts": now,
-        "hash": request_hash,
+        "hash": hash_param,
         "limit": "100",
         "offset": str(offset),
     }
-    response_string = requests.get(URL, params=params)
-    response_dict = json.loads(response_string.content.decode("utf-8"))
-    try:
-        return response_dict["data"]["results"]
-    except Exception:
-        print(response_dict)
-        return
+
+    response_string = requests.get(REQUEST_URL, params=params)
+    response_json = json.loads(response_string.content.decode("utf-8"))
+    return response_json["data"]["results"]
 
 
 if __name__ == "__main__":
     requests_cache.install_cache("marvel_cache", ignored_parameters=["hash", "ts"])
 
-    offset = 1300
-    characters = get_characters(offset)
-    ids = [character["id"] for character in characters]
-
-    while characters != []:
-        offset += 100
-        characters = get_characters(offset)
-        ids += [character["id"] for character in characters]
+    ids = [character["id"] for character in get_all_characters()]
 
     print(f"Found {len(ids)} ids")
     print(ids)
--- todo.md
+++ todo.md
@@ -1,14 +1,15 @@
 NOW
-- [x] Cache the IDs
+- [x] Do chunking properly
 
 TODO
+- [ ] Set cache expiry date
+- [ ] Remove hardcoding of 100 increment
+- [ ] Do logging properly
 - [ ] Setup basic API
-- [ ] Catch errors properly
-- [ ] Set cache to expire
-- [ ] Do chunking properly
 
 DONE
 - [x] Make a request to the Marvel API
 - [x] Get the first 100 character IDs
 - [x] Get all of the character IDs
 - [x] Remove duplication in successive requests
+- [x] Cache the IDs
